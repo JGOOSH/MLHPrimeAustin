7b5e22b8c7c2b302b8d1636c854680b884787d64
Gabriel Richardson <gabriel_richardson_97@hotmail.com>
2016-11-13 02:33:23 -0600

    Interface/UI update

---
+++ MLHAustin/app/src/main/java/com/daprlabs/aaron/swipedeck2/Messenger.java
@@ -0,0 +1,17 @@
+package com.daprlabs.aaron.swipedeck2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Gabriel Richardson on 11/13/2016.
+ */
+public class Messenger extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.messenger);
+        
+    }
+}
